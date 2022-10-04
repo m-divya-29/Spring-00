f9cb0599ff01b9bb8bb1abfe61a5b3dea0726991
m-divya-29 <divyamahankali29@gmail.com>
2022-10-04 17:57:57 +0530

    Constructor, Property injection

--- src/main/java/com/snow/spring/Draw.java
+++ src/main/java/com/snow/spring/Draw.java
@@ -1,16 +1,28 @@
 package com.snow.spring;
 
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.FileSystemResource;
 
 public class Draw {
     public static void main(String[] args) {
-        //Instantiation of bean factory
-        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+        /*  Instantiation of bean factory
+            BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+            Triangle t = (Triangle) factory.getBean("triangle");
+            t.draw();
+        */
+
+        //Application context
+        ApplicationContext applicationContext =
+                new ClassPathXmlApplicationContext("spring.xml");
         BeanFactory beanFactory = (BeanFactory) applicationContext;
         Triangle triangle = (Triangle) beanFactory.getBean("triangle");
         triangle.draw();
 
+        //Print the preset variable value
+       System.out.println(triangle.getType());
 
     }
 }
--- src/main/java/com/snow/spring/Triangle.java
+++ src/main/java/com/snow/spring/Triangle.java
@@ -1,6 +1,27 @@
 package com.snow.spring;
 
 public class Triangle {
+    private String type;
+    private int height;
+
+    Triangle(int height){
+        this.height = height;
+    }
+    Triangle(String type){
+        this.type = type;
+    }
+    Triangle(int height, String type){
+        this.height = height;
+        this.type = type;
+    }
+    public String getType() {
+        return type;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+
+
     void draw(){
         System.out.println("Triangle drawn");
     }
