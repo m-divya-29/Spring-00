b4a0498f306506f25f8259811f5e7e3665dff606
m-divya-29 <divyamahankali29@gmail.com>
2022-10-04 19:04:47 +0530

    Initializing collections

--- src/main/java/com/snow/spring/Triangle.java
+++ src/main/java/com/snow/spring/Triangle.java
@@ -1,35 +1,25 @@
 package com.snow.spring;
 
+import java.util.List;
+
 public class Triangle {
-    private Point pointA;
-    private Point pointB;
-    private Point pointC;
+    List<Point> points;
 
-    public Point getPointA() {
-        return pointA;
+    public List<Point> getPoints() {
+        return points;
     }
 
-    public void setPointA(Point pointA) {
-        this.pointA = pointA;
-    }
-
-    public Point getPointB() {
-        return pointB;
-    }
-
-    public void setPointB(Point pointB) {
-        this.pointB = pointB;
-    }
-
-    public Point getPointC() {
-        return pointC;
-    }
-
-    public void setPointC(Point pointC) {
-        this.pointC = pointC;
+    public void setPoints(List<Point> points) {
+        this.points = points;
     }
 
     void draw(){
-        System.out.println("Triangle drawn at: "+ pointA +", "+ pointB +", "+ pointC);
+        System.out.println("Triangle drawn at: ");
+        for (Point point :
+                points) {
+            System.out.print(point+" ");
+        }
+
+
     }
 }
