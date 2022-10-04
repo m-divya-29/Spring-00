7d0f60f57ebdc4862266abf7b54951c2d5f948c2
m-divya-29 <divyamahankali29@gmail.com>
2022-10-04 18:19:00 +0530

    Object injection

--- src/main/java/com/snow/spring/Draw.java
+++ src/main/java/com/snow/spring/Draw.java
@@ -1,28 +1,15 @@
 package com.snow.spring;
 
 import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.core.io.FileSystemResource;
 
 public class Draw {
     public static void main(String[] args) {
-        /*  Instantiation of bean factory
-            BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
-            Triangle t = (Triangle) factory.getBean("triangle");
-            t.draw();
-        */
-
-        //Application context
-        ApplicationContext applicationContext =
-                new ClassPathXmlApplicationContext("spring.xml");
-        BeanFactory beanFactory = (BeanFactory) applicationContext;
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        BeanFactory beanFactory = context;
         Triangle triangle = (Triangle) beanFactory.getBean("triangle");
+        // triangle.setPointA((Point)beanFactory.getBean("point1")); -->  instead of this, object injection can be done from spring.xml itself
         triangle.draw();
-
-        //Print the preset variable value
-       System.out.println(triangle.getType());
-
     }
 }
--- src/main/java/com/snow/spring/Triangle.java
+++ src/main/java/com/snow/spring/Triangle.java
@@ -1,28 +1,35 @@
 package com.snow.spring;
 
 public class Triangle {
-    private String type;
-    private int height;
+    private Point pointA;
+    private Point pointB;
+    private Point pointC;
 
-    Triangle(int height){
-        this.height = height;
-    }
-    Triangle(String type){
-        this.type = type;
-    }
-    Triangle(int height, String type){
-        this.height = height;
-        this.type = type;
-    }
-    public String getType() {
-        return type;
-    }
-    public void setType(String type) {
-        this.type = type;
+    public Point getPointA() {
+        return pointA;
     }
 
+    public void setPointA(Point pointA) {
+        this.pointA = pointA;
+    }
+
+    public Point getPointB() {
+        return pointB;
+    }
+
+    public void setPointB(Point pointB) {
+        this.pointB = pointB;
+    }
+
+    public Point getPointC() {
+        return pointC;
+    }
+
+    public void setPointC(Point pointC) {
+        this.pointC = pointC;
+    }
 
     void draw(){
-        System.out.println("Triangle drawn");
+        System.out.println("Triangle drawn at: "+ pointA +", "+ pointB +", "+ pointC);
     }
 }
